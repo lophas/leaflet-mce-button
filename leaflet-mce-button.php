f663a7fa2d4dbea911ca12cbe060465f34c02165
jewpi <nathan@jewpi.com>
2018-11-19 09:30:18 +0100

    Initial commit

---
+++ leaflet-mce-button.php
@@ -0,0 +1,27 @@
+<?php
+/*
+	Plugin Name: Leaflet Map MCE Button
+	Description:
+	Version: 0.6
+	Plugin URI: https://github.com/lophas/leaflet-mce-button
+	GitHub Plugin URI: https://github.com/lophas/leaflet-mce-button
+	Author: Attila Seres
+	Author URI:
+*/
+add_action('init', function() {
+	if(!current_user_can('edit_posts') && ! current_user_can('edit_pages')) return;
+	if(get_user_option('rich_editing') !== 'true') return;
+	add_filter('mce_external_plugins', function($plugin_array) {
+		if(shortcode_exists('leaflet-map')) {
+			$js = __DIR__.'/'.substr(basename(__FILE__),0,-4).'/plugin.js';
+      if(file_exists($js)) {
+        $plugin_array['leaflet'] = site_url().substr($js,strlen(ABSPATH)-1).'?'.filemtime($js);
+        add_filter('mce_buttons', function($buttons) {
+      		array_push($buttons, 'separator', 'leaflet');
+      		return $buttons;
+      	});
+      }
+		}
+		return $plugin_array;
+	});
+});
---
+++ leaflet-mce-button/plugin.js
@@ -0,0 +1,100 @@
+(function() {
+    tinymce.PluginManager.add('leaflet', function( editor, url ) {
+        editor.addButton( 'leaflet', {
+            text: 'Leaflet',
+            tooltip: 'Insert Leaflet Map',
+            icon: 'leaflet dashicons-before dashicons-location-alt',
+            onclick: function() {
+                editor.windowManager.open( {
+                    title: 'Leaflet Map',
+                    body: [
+                        {
+                            type   : 'textbox',
+                            name   : 'address',
+                            label  : 'Street Address',
+                            tooltip: 'Leave empty if Lat/Lon specified',
+                            value  : ''
+                        },
+                        {
+                            type   : 'textbox',
+                            name   : 'lat',
+                            label  : 'Latitude',
+                            tooltip: 'Leave empty if Street Address specified',
+                            value  : ''
+                        },
+                        {
+                            type   : 'textbox',
+                            name   : 'lon',
+                            label  : 'Longitude',
+                            tooltip: 'Leave empty if Street Address specified',
+                            value  : ''
+                        },
+                        {
+                            type   : 'textbox',
+                            name   : 'zoom',
+                            label  : 'Zoom',
+                            tooltip: 'Optional (0-20)',
+                            value  : ''
+                        },
+                        {
+                            type   : 'textbox',
+                            name   : 'width',
+                            label  : 'Width',
+                            tooltip: 'Optional',
+                            value  : ''
+                        },
+                        {
+                            type   : 'textbox',
+                            name   : 'height',
+                            label  : 'Height',
+                            tooltip: 'Optional',
+                            value  : ''
+                        },
+                        {
+                            type   : 'checkbox',
+                            name   : 'marker',
+                            label  : 'Marker',
+                            tooltip: 'Optional',
+                            value  : ''
+                          },
+                          {
+                              type   : 'textbox',
+                              name   : 'title',
+                              label  : 'Marker title',
+                              tooltip: 'Optional',
+                              value  : ''
+                        }
+                    ],
+                    onsubmit: function( e ) {
+                        shortcode = '[leaflet-map';
+                        if(e.data.address != '') {
+                          shortcode += ' address="' + e.data.address + '"';
+                        } else if(e.data.lat != '' && e.data.lon != '') {
+                          shortcode += ' lat="' + e.data.lat + '" lng="' + e.data.lon + '"';
+                        }
+                        if(e.data.zoom != '') {
+                          shortcode += ' zoom="' + e.data.zoom + '"';
+                        }
+                        if(e.data.width != '') {
+                          shortcode += ' width="' + e.data.width + '"';
+                        }
+                        if(e.data.height != '') {
+                          shortcode += ' height="' + e.data.height + '"';
+                        }
+                        shortcode += ']';
+                        if(e.data.marker) {
+                          shortcode += '[leaflet-marker';
+                          if(e.data.title != '') {
+                            shortcode += ' visible="true"]' + e.data.title + '[/leaflet-marker]';
+                          } else {
+                            shortcode += ']';
+                          }
+                        }
+                        editor.insertContent( shortcode );
+                    }
+                });
+            },
+        });
+    });
+
+})();
